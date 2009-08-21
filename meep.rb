8dc5868f0e6c457d3c8611d266c5cfa25797d0b2
James Lambie <jameslambie@gmail.com>
2009-08-22 00:18:50 +0100

    added all code and models

---
+++ meep.rb
@@ -0,0 +1,130 @@
+# meep.rb
+
+require 'rubygems'
+require 'sinatra'
+require 'rack-flash'
+require 'digest/md5'
+require 'json'
+require 'net/http'
+require 'net/https'
+
+require 'models'
+
+set :sessions, true
+use Rack::Flash
+
+['/', '/home'].each do |path|
+  get path do
+    if session[:user_id].nil? then erb :login
+    else redirect "/#{User.get(session[:user_id]).email}"
+    end
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+post '/login' do
+  openid_user = get_user(params[:token])
+  user = User.find(openid_user[:identifier])
+  user.update_attributes({:nickname => openid_user[:nickname], :email => openid_user[:email], :photo_url => "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(openid_user[:email])}"}) if user.new_record?
+  session[:user_id] = user.id
+  redirect "/#{user.email}"
+end
+
+post '/meep' do
+  user = User.get(session[:user_id])
+  Meep.create(:text => params[:meep], :user => user)
+  redirect "/#{user.email}"
+end
+
+['/follows', '/followers'].each do |path|
+  get path do
+    @myself = User.get(session[:user_id])
+    @dm_count = dm_count
+    erb :follows
+  end
+end
+
+get '/:email' do
+  @myself = User.get(session[:user_id])
+  @user = @myself.email == params[:email] ? @myself : User.first(:email => params[:email])
+  @dm_count = dm_count
+  erb :home
+end
+
+get '/follow/:email' do
+  Relationship.create(:user => User.first(:email => params[:email]), :follower => User.get(session[:user_id]))
+  redirect '/home'
+end
+
+delete '/follows/:user_id/:follows_id' do
+  r = Relationship.first(:follower_id => params[:user_id], :user_id => params[:follows_id])
+  r.destroy
+  redirect '/follows'
+end
+
+get '/direct_messages/:dir' do
+  @myself = User.get(session[:user_id])
+  case params[:dir]
+  when 'received' then @meeps = Meep.all(:recipient_id => @myself.id)
+  when 'sent' then @meeps = Meep.all(:user_id => @myself.id, :recipient_id.not => nil)
+  end
+  
+  @dm_count = dm_count
+  erb :direct_messages
+end
+
+error do
+  flash[:error] = env['sinatra.error'].to_s
+  redirect '/home'
+end
+
+# processing helpers
+def dm_count
+  Meep.count(:recipient_id => session[:user_id]) + Meep.count(:user_id => session[:user_id], :recipient_id.not => nil)
+end
+
+def get_user(token)
+  u = URI.parse('https://rpxnow.com/api/v2/auth_info')
+  
+  req = Net::HTTP::Post.new(u.path)
+  req.set_form_data({'token' => token, 'apiKey' => '5bc712009a3220abf2860f2178198cd432a73fba', 'format' => 'json', 'extended' => 'true'})
+  
+  http = Net::HTTP.new(u.host, u.port)
+  http.use_ssl = true if u.scheme == 'https'
+  
+  json = JSON.parse(http.request(req).body)
+  
+  if json['stat'] == 'ok'
+    identifier = json['profile']['identifier']
+    nickname = json['profile']['preferredUsername']
+    nickname = json['profile']['displayName'] if nickname.nil?
+    email = json['profile']['email']
+    {:identifier => identifier, :nickname => nickname, :email => email}
+  else
+    raise LoginFailedError, 'Cannot log in. Try another account!'
+  end
+end
+
+# template helpers
+helpers do
+  def time_ago_in_words(timestamp)
+    minutes = (((Time.now - timestamp).abs)/60).round
+    return nil if minutes < 0
+ 
+    case minutes
+    when 0 then 'less than a minute ago'
+    when 0..4 then 'less than 5 minutes ago'
+    when 5..14 then 'less than 15 minutes ago'
+    when 15..29 then 'less than 30 minutes ago'
+    when 30..59 then 'more than 30 minutes ago'
+    when 60..119 then 'more than 1 hour ago'
+    when 120..239 then 'more than 2 hours ago'
+    when 240..479 then 'more than 4 hours ago'
+    else timestamp.strftime('%I:%M %p %d-%b-%Y')
+    end
+  end
+end
---
+++ models.rb
@@ -0,0 +1,106 @@
+require 'rubygems'
+require 'dm-core'
+require 'dm-timestamps'
+require 'dm-aggregates'
+require 'open-uri'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'mysql://localhost/meep')
+
+class User
+  include DataMapper::Resource
+  
+  property :id,         Serial
+  property :email,      String, :length => 255
+  property :nickname,   String, :length => 255
+  property :identifier, String, :length => 255
+  property :photo_url,  String, :length => 255
+  
+  has n, :meeps
+  has n, :direct_messages, :class_name => "Meep"
+  has n, :relationships
+  has n, :followers, :through => :relationships, :class_name => "User", :child_key => [:user_id]
+  has n, :follows, :through => :relationships, :class_name => "User", :remote_name => :user, :child_key => [:follower_id]
+  
+  def self.find(identifier)
+    u = first(:identifier => identifier)
+    u = new(:identifier => identifier) if u.nil?
+    return u
+  end
+  
+  def displayed_meeps
+    meeps = []
+    meeps += self.meeps.all(:recipient_id => nil, :limit => 10, :order => [:created_at.desc])
+    self.follows.each do |f| meeps += f.meeps.all(:recipient_id => nil, :limit => 10, :order => [:created_at.desc]) end if @myself == @user
+    meeps.sort! { |x,y| y.created_at <=> x.created_at }
+    meeps[0..10]
+  end
+  
+end
+
+class Relationship
+  include DataMapper::Resource
+  
+  property :user_id,      Integer, :key => true
+  property :follower_id,  Integer, :key => true
+  
+  belongs_to :user, :child_key => [:user_id]
+  belongs_to :follower, :class_name => "User", :child_key => [:follower_id]
+end
+
+class Meep
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :text,       String, :length => 140
+  property :created_at, DateTime
+  
+  belongs_to :recipient, :class_name => "User", :child_key => [:recipient_id]
+  belongs_to :user
+  
+  before :save do
+    case
+    when starts_with?('dm ')
+      process_dm
+    when starts_with?('follow ')
+      process_follow
+    else
+      process
+    end
+  end
+  
+  # general scrubbing of meep
+  def process
+    # process url
+    urls = self.text.scan(URL_REGEXP)
+    urls.each { |url|
+      tiny_url = open("http://tinyurl.com/api-create.php?url=#{url[0]}") { |s| s.read }
+      self.text.sub!(url[0], "<a href='#{tiny_url}'>#{tiny_url}</a>")
+    }
+    
+    # process @
+    ats = self.text.scan(AT_REGEXP)
+    ats.each { |at| 
+      self.text.sub!(at, "<a href='/#{at[2,at.length]}'>#{at}</a>")
+    }
+  end
+  
+  def process_dm
+    self.recipient = User.first(:email => self.text.split[1])
+    self.text = self.text.split[2..self.text.split.size].join(' ') # remove the first two words
+    process
+  end
+  
+  def process_follow
+    Relationship.create(:user => User.first(:email => self.text.split[1]), :follower => self.user)
+    throw :halt # don't save
+  end
+  
+  def starts_with?(prefix)
+    prefix = prefix.to_s
+    self.text[0..prefix.length] == prefix
+  end
+    
+end
+
+URL_REGEXP = Regexp.new('\b ((https?|telnet|gopher|file|wais|ftp) : [\w/#~:.?+=&%@!\-] +?) (?=[.:?\-] * (?: [^\w/#~:.?+=&%@!\-]| $ ))', Regexp::EXTENDED)
+AT_REGEXP = Regexp.new('\s@[\w.@_-]+', Regexp::EXTENDED)
